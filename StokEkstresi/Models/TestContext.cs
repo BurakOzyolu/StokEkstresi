56af181775f8642d99dd96f7ef4fcdd19e2a3310
Burak Özyolu <33603280+BurakOzyolu@users.noreply.github.com>
2023-01-24 01:46:51 +0300

    EntityFramework DatabaseFirst

---
+++ StokEkstresi/Models/TestContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace StokEkstresi.Models
+{
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Sti> Stis { get; set; }
+        public virtual DbSet<Stk> Stks { get; set; }
+        public virtual DbSet<View1> View1s { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-02GR6O4;Database=Test;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<Sti>(entity =>
+            {
+                entity.HasKey(e => new { e.EvrakNo, e.Tarih, e.IslemTur })
+                    .HasName("pkSTI");
+
+                entity.ToTable("STI");
+
+                entity.Property(e => e.EvrakNo)
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Birim)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fiyat).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalKodu)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Miktar).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Tutar).HasColumnType("numeric(25, 6)");
+            });
+
+            modelBuilder.Entity<Stk>(entity =>
+            {
+                entity.HasKey(e => e.MalKodu)
+                    .HasName("pkSTK");
+
+                entity.ToTable("STK");
+
+                entity.Property(e => e.MalKodu)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalAdi)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<View1>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("View_1");
+
+                entity.Property(e => e.EvrakNo)
+                    .IsRequired()
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalKodu)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Miktar).HasColumnType("numeric(25, 6)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ StokEkstresi/Models/View1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StokEkstresi.Models
+{
+    public partial class View1
+    {
+        public short IslemTur { get; set; }
+        public string EvrakNo { get; set; }
+        public int Tarih { get; set; }
+        public string MalKodu { get; set; }
+        public decimal Miktar { get; set; }
+        public int Id { get; set; }
+        public long? SiraNo { get; set; }
+    }
+}
